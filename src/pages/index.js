a689b6798462de0d2cdaa0b7edecfe90a6eeba0a
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-11-13 20:31:22 +0530

    Basic setup of Aiox

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Base } from '../components/Base';
 import { Button } from 'react-bootstrap';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 
 export const Index = () => {
   function showSuccessToast(){
     // toast.success("Success Toast");
     toast.error("Error Occured");
-  
+    
+  }
+
+  function getDataFromServer(){
+    toast.success("Fatching Data From Server");
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((data)=>{
+      console.log(data.data);
+      console.log("data");
+
+      toast.success("Request Done!!")
+    }).catch((erroe)=>{
+      console.log("Something went wrong");
+      toast.error("Someyhing went wrong!!");
+    })
   }
   return (
 
@@ -16,6 +30,7 @@ export const Index = () => {
       <h1>Working on Home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium deleniti perspiciatis voluptates? Illum odit eos fugiat, sapiente deserunt suscipit tenetur maxime ad ipsam explicabo minima, architecto vitae, sequi nam aut.</p>
       <Button variant='success' onClick={showSuccessToast}>Toasty Example</Button>
+      <Button variant='warning' onClick={getDataFromServer}>Get Data From Server</Button>
     </Base>
   );
 }
