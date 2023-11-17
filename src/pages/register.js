ad9a57c035f3da86f8a7671a3e7f285c87b34394
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-11-17 10:25:27 +0530

    Handling signup data

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,14 +4,51 @@ import { Button, Card, CardBody, Col, Container, Form, Row } from 'react-bootstr
 import logo from '../assets/logo.png'
 
 export const Register = () => {
- 
+  let[data, setData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+    gender:'',
+    about:''
+  });
+
+  const handleChange=(event, property)=>{
+    setData({
+      ...data,
+    [property] : event.target.value
+    })
+  }
+
+  // Clear Data
+  const clearData=()=>{
+    setData({
+      name:'',
+      email:'',
+      password:'',
+      confirmPassword:'',
+      gender:'',
+      about:''
+    })
+  }
+
+  const[errorData, setErrorData] = useState({
+    isError:false,
+    errorData:null
+  })
+
   function registerForm() {
     return (
       <Container>
-       
+        {/* {JSON.stringify(data)} */}
         <Row>
           <Col sm={{ span: 8, offset: 2 }}>
-            <Card className='my-2 border-0 shadow p-5'>
+            <Card className='my-2 border-0 shadow p-5' style={
+             {
+              position:'relative',
+              top:-70
+             }
+            } >
               <CardBody>
                 <Container className='text-center'>
                   <img src={logo} height={70} width={70}></img>
@@ -20,19 +57,31 @@ export const Register = () => {
                 <Form>
                   <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>Enter Your Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" />
+                    <Form.Control type="text" placeholder="Enter Name" 
+                    onChange={(event)=> handleChange(event,'name')}
+                    value={data.name}
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Enter your Email </Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Label>Enter your Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"
+                     onChange={(event)=> handleChange(event,'email')}
+                     value={data.email}
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formPassword">
                     <Form.Label>Enter new Password </Form.Label>
-                    <Form.Control type="password" placeholder="Enter Password" />
+                    <Form.Control type="password" placeholder="Enter Password"
+                    onChange={(event)=> handleChange(event,'password')}
+                    value={data.password}
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formConfirmPassword">
                     <Form.Label>Re Enter Password </Form.Label>
-                    <Form.Control type="password" placeholder="Re Enter Password" />
+                    <Form.Control type="password" placeholder="Re Enter Password"
+                    onChange={(event)=> handleChange(event,'confirmPassword')}
+                    value={data.confirmPassword}
+                    />
                   </Form.Group>
                   <Form.Group className='mb-3'>
                   <Form.Label>Select Gender </Form.Label>
@@ -43,6 +92,10 @@ export const Register = () => {
                       type={'radio'}
                       label='Male'
                       id={'gender'}
+                      value={'male'}
+                      checked={data.gender=='male'}
+                      onChange={(event)=> handleChange(event,'gender')}
+                     
                     />
                      <Form.Check
                      inline
@@ -50,12 +103,19 @@ export const Register = () => {
                       type={'radio'}
                       label='Female'
                       id={'gender'}
+                      value={'female'}
+                      checked={data.gender=='female'}
+                      onChange={(event)=> handleChange(event,'gender')}
+                      
                     />
                    </div>
                   </Form.Group>
                   <Form.Group className='mb-2'>
                     <Form.Label>Write something about yourself</Form.Label>
-                    <Form.Control as={`textarea`} rows={6} placeholder='Write here'/>
+                    <Form.Control as={`textarea`} rows={6} placeholder='Write here'
+                    onChange={(event)=> handleChange(event,'about')}
+                    value={data.about}
+                    />
                   </Form.Group>
                 </Form>
                 <Container>
@@ -63,7 +123,7 @@ export const Register = () => {
                 </Container>
                 <Container className='text-center'>
                   <Button className='text-uppercase' variant='success'>Register</Button>
-                  <Button className='ms-2 text-uppercase' variant='danger'>Reset</Button>
+                  <Button className='ms-2 text-uppercase' variant='danger' onClick={clearData}>Reset</Button>
                 </Container>
               </CardBody>
             </Card>
